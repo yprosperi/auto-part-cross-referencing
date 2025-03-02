bd0d2551ed7da5d89263dd527cb35b36c54a84b2
APCRT25 <jcabrera64@mail.valenciacollege.edu>
2025-03-02 01:36:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Load data from updated JSON file
 let partsData = [];
-fetch('assets/car_parts_data_updated.json') // Ensure JSON is in /assets/
+fetch('https://raw.githubusercontent.com/yprosperi/auto-part-cross-referencing/main/assets/car_parts_data_updated.json') // Ensure JSON is in /assets/
     .then(response => response.json())
     .then(data => partsData = data)
     .catch(error => console.error('Error loading parts data:', error));
 
-// Function to handle the search
+// Function to handle the search and open a new page
 function searchParts() {
     const searchInput = document.getElementById('searchInput').value.toUpperCase();
     const sortOption = document.getElementById('sortOption').value;
 
-    // Redirect to results page with query parameters
-    window.location.href = `results.html?query=${encodeURIComponent(searchInput)}&sort=${encodeURIComponent(sortOption)}`;
+    // Redirect to results page with query parameters in a new tab
+    window.open(`results.html?query=${encodeURIComponent(searchInput)}&sort=${encodeURIComponent(sortOption)}`, '_blank');
 }
